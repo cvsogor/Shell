79bf82ccbf186878a8be53a8256e4c4b8a072cd4
cvsogor <cvsogor@gmail.com>
2025-08-22 15:08:17 +0100

    code grooming

--- main.py
+++ main.py
@@ -2,15 +2,27 @@ from typing import Union
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 import requests
+import json
 
 class Convert(BaseModel):
     ccy_from: str
     ccy_to: str
     quantity: float
 
+# FAKE Class
 class FXRates():
     def get_rate(self, pair):
-        #GET request with parameter “?ccy_pair=GBPUSD”
+        """
+        url = "http://fx.com/fxrates?ccy_pair=GBPUSD"
+        try:
+            response = requests.get(url)
+            response.raise_for_status()  
+            resp = response.json()
+            rate = json.loads(resp)
+            return rate["rate"]
+        except requests.exceptions.RequestException as e:
+            return 0.0
+        """
         pairs = {"EURUSD": 1.16, "GBPUSD": 1.34, "USDCAD": 1.39, "USDJPY": 148.71, "EURGBP": 0.86}
         if pair in pairs:
             rate = pairs[pair]
@@ -33,8 +45,10 @@ def get_rate(ccy_from, ccy_to):
     pair = ccy_from + ccy_to
     rate = fx.get_rate(pair)
     if rate == 0:
-        rate1 = fx.get_rate(ccy_from + "USD")
-        rate2 = fx.get_rate("USD" + ccy_to)
+        #triangulation rule via USD
+        usd = "USD"
+        rate1 = fx.get_rate(ccy_from + usd)
+        rate2 = fx.get_rate(usd + ccy_to)
         if (rate1 != 0) and (rate2 != 0):
             rate = rate1 * rate2
     return rate
@@ -42,6 +56,9 @@ def get_rate(ccy_from, ccy_to):
 
 @app.post("/converter/")
 def read_item(convert: Convert):
+    if convert.quantity <= 0:
+        raise HTTPException(status_code=400, detail="Quantity should be more than 0")
+    
     rate = get_rate(convert.ccy_from, convert.ccy_to)
     if rate == 0:
         rate = get_rate(convert.ccy_to, convert.ccy_from)
